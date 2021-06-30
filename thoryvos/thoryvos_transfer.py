81cb8769969ae6aa73c52cf8d61ac633156fc84b
freakingrocky <rakshan793@gmail.com>
2021-07-01 01:04:26 +0530

    Update thoryvos_transfer.py

--- thoryvos/thoryvos_transfer.py
+++ thoryvos/thoryvos_transfer.py
@@ -6,20 +6,20 @@ from os import getcwd
 def upload(infile):
     """Upload the output file to anonfiles and returns a link."""
     anon = AnonFile()
-    status, file_url = anon.upload_file(infile)
-    return file_url
+    upload_obj = anon.upload_file(infile, progressbar=False)
+    return upload_obj.geturl()
 
 
 def download(url):
     """Download a file from anonfiles."""
     anon = AnonFile()
     location = getcwd()
-    anon.download_file(url, location)
-    return location
+    filename = anon.download_file(url, path=location)
+    return location + filename
 
 
 def verify(text):
-    """Verify that the url is valid in a very simple manners."""
+    """Verify that the url is valid in a very simple manner."""
     if 'anonfiles.com' in text:
         return True
     return False
